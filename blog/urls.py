be6f5449fd296b8180667ef9a18d9e62bc46e3b5
xmush <xmushdev@gmail.com>
2021-10-05 10:43:23 +0700

    add suburl in app managing route

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('', views.index)
+    url('', views.index),
+    url('loadmore/', views.loadmore),
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request) :
     return render(request, 'blog/index.html')
+
+def loadmore(request) :
+    return render(request, 'blog/loadmore.html')
